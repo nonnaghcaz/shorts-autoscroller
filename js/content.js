0ff3428d9b14cd61b523b4d9995ca0ec56787c30
Zach Gannon <7028393+NonnagHcaz@users.noreply.github.com>
2025-01-20 21:50:06 -0500

    Added ad skipping

--- js/content.js
+++ js/content.js
@@ -4,13 +4,18 @@ console.log("Content script loaded");
 const SHORTS_PLAYER_XPATH = '//*[@id="shorts-player"]';
 const SHORTS_PLAYER_VIDEO_XPATH = `${SHORTS_PLAYER_XPATH}//video`;
 const NAVIGATION_BUTTON_DOWN_XPATH = '//*[@id="navigation-button-down"]//button';
-const INTERVAL_MS = 1000;
+const SPONSORED_BADGE_XPATH = '//*[contains(@class, "badge-shape-wiz__text")]';
+const INTERVAL_MS = 100;
 
 checkIfVideoEnded();
 
+var sinceSponsored = 0;
+var wasJustSponsored = false;
+
+
 function checkIfVideoEnded() {
-    // const currentUrl = getCurrentUrl();
     var endOnNext = false;
+    var originalUrl = getCurrentUrl();
     const checkInterval = setInterval(() => {
         waitForXpath(SHORTS_PLAYER_VIDEO_XPATH).then((videoElement) => {
             if (!videoElement) {
@@ -18,20 +23,37 @@ function checkIfVideoEnded() {
                 clearInterval(checkInterval);
                 return;
             }
-            
+            var currentUrl = getCurrentUrl();
+
+            if (!currentUrl.includes("/shorts/")) {
+                console.log("Not a shorts video");
+                clearInterval(checkInterval);
+                return;
+            }
+
+            var sponsoredBadge = findElementByXpath(SPONSORED_BADGE_XPATH);
+            if (sponsoredBadge && sinceSponsored > 0) {
+                console.log("Sponsored badge found, since last ", sinceSponsored);
+                endOnNext = true;
+                wasJustSponsored = true;
+                sinceSponsored = 0;
+            }
+
             var videoDuration = videoElement.duration;
             var currentTime = videoElement.currentTime;
-            // var urlHasChanged = currentUrl !== getCurrentUrl();
-            // if (urlHasChanged) {
-            //     console.log("URL has changed, stopping video check");
-            //     clearInterval(checkInterval);
-            //     return;
-            // }
+            if (originalUrl !== currentUrl && !sponsoredBadge) {
+                console.log(`Url changed from ${originalUrl} to ${currentUrl}`);
+                endOnNext = false;
+                _handleSinceSponsored();
+                clearInterval(checkInterval);
+                return checkIfVideoEnded();
+            }
             var remainingTime = videoDuration - currentTime;
             if (endOnNext || videoElement.ended) {
+                endOnNext = false;
                 clearInterval(checkInterval);
-                onVideoEnded();
-            } else if (remainingTime <= INTERVAL_MS / 1000 + 0.1) {
+                return onVideoEnded();
+            } else if (remainingTime <= (INTERVAL_MS / 1000) * 1.01) {
                 console.log("Video is about to end");
                 endOnNext = true;
             }
@@ -39,15 +61,44 @@ function checkIfVideoEnded() {
     }, INTERVAL_MS);
 }
 
+function _handleSinceSponsored() {
+    var sponsoredBadge = findElementByXpath(SPONSORED_BADGE_XPATH);
+    if (!sponsoredBadge) {
+        wasJustSponsored = false;
+        sinceSponsored++;
+    }
+    console.log("Since sponsored: ", sinceSponsored);
+}
+
 function onVideoEnded() {
     console.log("Video ended");
+    var currentUrl = getCurrentUrl();
     const nextButton = findElementByXpath(NAVIGATION_BUTTON_DOWN_XPATH);
     if (nextButton) {
         nextButton.click();
-        checkIfVideoEnded();
+        waitForUrlChange(currentUrl).then(() => {
+            _handleSinceSponsored();
+            return checkIfVideoEnded();
+        });
     } else {
         console.log("No next button found");
     }
+}
+
+function findElementsByVisibleText(text) {
+    return findElementsByXpath(`//*[contains(text(), "${text}")]`);
+}
+
+function waitForUrlChange(originalUrl) {
+    return new Promise((resolve) => {
+        const checkInterval = setInterval(() => {
+            var currentUrl = getCurrentUrl();
+            if (currentUrl !== originalUrl) {
+                clearInterval(checkInterval);
+                resolve();
+            }
+        }, INTERVAL_MS);
+    });
 }
 
 function findElementsByXpath(path) {
