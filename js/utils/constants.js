f2cf6532ab2b2d397ef8346daaaef03b167e3b79
Zach Gannon <7028393+NonnagHcaz@users.noreply.github.com>
2025-01-26 12:40:15 -0500

    Abstracted constants and dom functions to own modules

---
+++ js/utils/constants.js
@@ -0,0 +1,8 @@
+const SHORTS_PLAYER_XPATH = '//*[@id="shorts-player"]';
+const SHORTS_PLAYER_VIDEO_XPATH = `${SHORTS_PLAYER_XPATH}//video`;
+const NAVIGATION_BUTTON_DOWN_XPATH = '//*[@id="navigation-button-down"]//button';
+const SPONSORED_BADGE_XPATH = '//*[contains(@class, "badge-shape-wiz__text")]';
+const INTERVAL_MS = 100;
+
+
+export { INTERVAL_MS, NAVIGATION_BUTTON_DOWN_XPATH, SHORTS_PLAYER_VIDEO_XPATH, SHORTS_PLAYER_XPATH, SPONSORED_BADGE_XPATH };
---
+++ js/utils/dom.js
@@ -0,0 +1,89 @@
+function findElementsByVisibleText(text) {
+    return findElementsByXpath(`//*[contains(text(), "${text}")]`);
+}
+
+function waitForUrlChange(originalUrl) {
+    return new Promise((resolve) => {
+        const checkInterval = setInterval(() => {
+            var currentUrl = getCurrentUrl();
+            if (currentUrl !== originalUrl) {
+                clearInterval(checkInterval);
+                resolve();
+            }
+        }, INTERVAL_MS);
+    });
+}
+
+function findElementsByXpath(path) {
+    return document.evaluate(
+      path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  }
+  
+function findElementByXpath(path) {
+    return document.evaluate(
+        path, 
+        document, 
+        null, 
+        XPathResult.FIRST_ORDERED_NODE_TYPE, 
+        null
+    ).singleNodeValue;
+}
+
+function waitForXpath(xpath) {
+    return new Promise((resolve) => {
+        if (findElementByXpath(xpath)) {
+            return resolve(findElementByXpath(xpath));
+        }
+    
+        const observer = new MutationObserver((mutations) => {
+            if (findElementByXpath(xpath)) {
+                resolve(findElementByXpath(xpath));
+                observer.disconnect();
+            }
+        });
+    
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+}
+
+function waitForElement(selector) {
+    return new Promise((resolve) => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+    
+        const observer = new MutationObserver((mutations) => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+    
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+}
+
+function getCurrentUrl() {
+    return window.location.href;
+}
+
+function isCurrentUrl(url) {
+    return getCurrentUrl() === url;
+}
+
+function hasUrlChanged(originalUrl) {
+    return getCurrentUrl() !== originalUrl;
+}
+
+
+
+export {
+    findElementByXpath, findElementsByVisibleText, findElementsByXpath, getCurrentUrl, hasUrlChanged, isCurrentUrl, waitForElement, waitForUrlChange, waitForXpath
+};
+
